fdf13dcb6e0e67ccb13752878fb8ac28a1953584
EvgKrsk24 <chiaevgkrsk@gmail.com>
2022-10-20 10:01:26 +0700

    Update LpoController to RESTController; Add configureMessageConverters to WebConfig

--- src/main/java/ru/czl/lpo/server/config/WebConfig.java
+++ src/main/java/ru/czl/lpo/server/config/WebConfig.java
@@ -1,14 +1,29 @@
 package ru.czl.lpo.server.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Collections;
+import java.util.List;
 
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("ru.czl.lpo.server")
 public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter=new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+        converters.add(converter);
+
+    }
 }
--- src/main/java/ru/czl/lpo/server/controller/LpoController.java
+++ src/main/java/ru/czl/lpo/server/controller/LpoController.java
@@ -1,19 +1,27 @@
 package ru.czl.lpo.server.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.czl.lpo.server.entity.InfoDistrict;
 
-@Controller
+@RestController
 @RequestMapping("/lpo")
 public class LpoController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getLpo(ModelMap model) {
-        return "My lpo";
+    public InfoDistrict getInfoDistrict() {
+        return createMockInfoDistrict();
+    }
+
+    private InfoDistrict createMockInfoDistrict() {
+        InfoDistrict infoDistrict= new InfoDistrict();
+        infoDistrict.getId();
+        infoDistrict.getName();
+        return infoDistrict;
     }
 
 }
